bccc2c39365981dcf7c701a3e33411261fe69fb7
Emily Well Jimenez and Michael Arrington <michael@>
2017-09-12 14:47:16 -0700

    build talking page with js functions.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Hola!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>BYEEEEEE~</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("button#stfu").click(function() {
+    $("ul#user").prepend("<li>Shut up!</li>");
+    $("ul#webpage").prepend("<li>No, u.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
